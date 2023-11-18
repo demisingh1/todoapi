af842b1a9d16a6c06c4948582c26401ae40afe70
demisingh1 <jaswinder.cyrus1@gmail.com>
2023-11-18 19:18:16 +0530

    cors updated

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 require('express-async-errors');
 require('./databaseConnection')
 const express = require('express');
+const cors = require('cors')
 const routes = require('./routes/taskRoutes');
 const connectDB = require('./databaseConnection');
 const app = express();
 
 // middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 // error middleware
